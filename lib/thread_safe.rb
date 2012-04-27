dfee5c65ad8dae6adad86b6630754c7747b8e824
Charles Oliver Nutter <headius@headius.com>
2012-04-27 09:58:18 -0500

    Add some docs.

--- lib/thread_safe.rb
+++ lib/thread_safe.rb
@@ -4,15 +4,27 @@ if defined?(JRUBY_VERSION)
   require 'jruby/synchronized'
   
   module ThreadSafe
+    # A thread-safe subclass of Array. This version locks
+    # against the object itself for every method call,
+    # ensuring only one thread can be reading or writing
+    # at a time. This includes iteration methods like
+    # #each.
     class Array < ::Array
       include JRuby::Synchronized
     end
   
+    # A thread-safe subclass of Hash. This version locks
+    # against the object itself for every method call,
+    # ensuring only one thread can be reading or writing
+    # at a time. This includes iteration methods like
+    # #each.
     class Hash < ::Hash
       include JRuby::Synchronized
     end
   end
 else
+  # Because MRI never runs code in parallel, the existing
+  # non-thread-safe structures should usually work fine.
   module ThreadSafe
     Array = ::Array
     Hash = ::Hash
