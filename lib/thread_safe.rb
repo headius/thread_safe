5d15e8d3746ab446a82a608c315d394d539aa309
thedarkone <thedarkone2@gmail.com>
2012-08-15 20:11:09 +0200

    Experimental support for TS::Array/Hash on rbx.

--- lib/thread_safe.rb
+++ lib/thread_safe.rb
@@ -28,5 +28,33 @@ module ThreadSafe
     # non-thread-safe structures should usually work fine.
     Array = ::Array
     Hash  = ::Hash
+  elsif defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+    require 'monitor'
+
+    class Hash < ::Hash; end
+    class Array < ::Array; end
+
+    [Hash, Array].each do |klass|
+      klass.class_eval do
+        private
+        def _mon_initialize
+          @_monitor = Monitor.new unless @_monitor # avoid double initialisation
+        end
+
+        def self.allocate
+          obj = super
+          obj.send(:_mon_initialize)
+          obj
+        end
+      end
+
+      klass.superclass.instance_methods(false).each do |method|
+        klass.class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+          def #{method}(*args)
+            @_monitor.synchronize { super }
+          end
+        RUBY_EVAL
+      end
+    end
   end
 end
